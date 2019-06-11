b633f668006bc6a0f608072900cb8e2865b42ad9
George <sonicgd@gmail.com>
2019-06-11 18:16:01 +0500

    start reworking front

--- SitkoCRM/ClientApp/src/components/Layout.js
+++ SitkoCRM/ClientApp/src/components/Layout.js
@@ -1,12 +1,48 @@
 import React from 'react';
-import { Container } from 'reactstrap';
-import NavMenu from './NavMenu';
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Grid from "@material-ui/core/Grid";
+import SearchIcon from "@material-ui/core/SvgIcon/SvgIcon";
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
+import Tooltip from "@material-ui/core/Tooltip";
+import IconButton from "@material-ui/core/IconButton";
+import Paper from "@material-ui/core/Paper";
+import RefreshIcon from '@material-ui/icons/Refresh';
 
 export default props => (
-  <div>
-    <NavMenu />
-    <Container>
-      {props.children}
-    </Container>
-  </div>
+    <Paper className={props.paper}>
+        <AppBar className={props.searchBar} position="static" color="default" elevation={0}>
+            <Toolbar>
+                <Grid container spacing={2} alignItems="center">
+                    <Grid item>
+                        <SearchIcon className={props.block} color="inherit"/>
+                    </Grid>
+                    <Grid item xs>
+                        <TextField
+                            fullWidth
+                            placeholder="Search by email address, phone number, or user UID"
+                            InputProps={{
+                                disableUnderline: true,
+                                className: props.searchInput,
+                            }}
+                        />
+                    </Grid>
+                    <Grid item>
+                        <Button variant="contained" color="primary" className={props.addUser}>
+                            Add user
+                        </Button>
+                        <Tooltip title="Reload">
+                            <IconButton>
+                                <RefreshIcon className={props.block} color="inherit"/>
+                            </IconButton>
+                        </Tooltip>
+                    </Grid>
+                </Grid>
+            </Toolbar>
+        </AppBar>
+        <div className={props.contentWrapper}>
+            {props.children}
+        </div>
+    </Paper>
 );
--- SitkoCRM/ClientApp/src/components/Navigator.js
+++ SitkoCRM/ClientApp/src/components/Navigator.js
@@ -1,5 +1,4 @@
 ﻿import React from 'react';
-import PropTypes from 'prop-types';
 import clsx from 'clsx';
 import { withStyles } from '@material-ui/core/styles';
 import Divider from '@material-ui/core/Divider';
@@ -137,9 +136,5 @@ function Navigator(props) {
         </Drawer>
     );
 }
-
-Navigator.propTypes = {
-    classes: PropTypes.object.isRequired,
-};
 
 export default withStyles(styles)(Navigator);
